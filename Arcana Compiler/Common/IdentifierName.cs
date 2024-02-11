18764afe107aabecee830ca28ce09c64fdd40146
Alex Zukowski <ziamor23@gmail.com>
2024-02-11 16:32:04 -0500

    Some renaming in IdentifierName

--- Arcana Compiler/Common/IdentifierName.cs
+++ Arcana Compiler/Common/IdentifierName.cs
@@ -1,39 +1,39 @@
 ﻿namespace Arcana_Compiler.Common {
     public class IdentifierName {
         public static readonly IdentifierName DefaultNameSpace = new IdentifierName(new List<string> { "Default" });
-        public List<string> Parts { get; private set; }
+        public List<string> AccessPath { get; private set; }
 
-        public string Identifier => Parts.Last();
-        public IdentifierName Qualifiers => new IdentifierName(Parts.Take(Parts.Count - 1).ToList());
+        public string Identifier => AccessPath.Last();
+        public IdentifierName Qualifiers => new IdentifierName(AccessPath.Take(AccessPath.Count - 1).ToList());
 
-        public bool IsFullyQualified => Parts.Count > 1;
+        public bool IsFullyQualified => AccessPath.Count > 1;
 
         public IdentifierName(string name) {
-            Parts = [name];
+            AccessPath = [name];
         }
 
-        public IdentifierName(List<string> parts) {
-            Parts = parts;
+        public IdentifierName(List<string> accessPath) {
+            AccessPath = accessPath;
         }
 
         public static IdentifierName operator +(IdentifierName identifierName, string addition) {
-            List<string> newParts = new List<string>(identifierName.Parts) { addition };
+            List<string> newParts = new List<string>(identifierName.AccessPath) { addition };
             return new IdentifierName(newParts);
         }
 
         public override string ToString() {
-            return string.Join(".", Parts);
+            return string.Join(".", AccessPath);
         }
 
         public override bool Equals(object? obj) {
             return obj is IdentifierName other &&
-                   Parts.SequenceEqual(other.Parts);
+                   AccessPath.SequenceEqual(other.AccessPath);
         }
 
         public override int GetHashCode() {
             unchecked {
                 int hash = 17;
-                foreach (var part in Parts) {
+                foreach (var part in AccessPath) {
                     hash = hash * 31 + (part != null ? part.GetHashCode() : 0);
                 }
                 return hash;
