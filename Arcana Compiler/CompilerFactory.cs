1cab828afce9e3603431379e5d48970b64cd4594
Alex Zukowski <ziamor23@gmail.com>
2024-02-18 17:48:36 -0500

    Breakup CompilerFactory.CreateCompiler into smaller methods

--- Arcana Compiler/CompilerFactory.cs
+++ Arcana Compiler/CompilerFactory.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.IO;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Arcana_Compiler.ArcanaModule;
 using Arcana_Compiler.ArcanaLexer;
 using Arcana_Compiler.ArcanaParser;
@@ -11,40 +9,55 @@ using Arcana_Compiler.ArcanaModule.Arcana_Compiler.ArcanaModule;
 
 public static class CompilerFactory {
     public static Compiler CreateCompiler(string configFilePath) {
-        // Build configuration
+        IConfigurationRoot configuration = BuildConfiguration(configFilePath);
+        IModule rootModule = LoadModule(configuration);
+        return BuildCompiler(rootModule);
+    }
+
+    private static IConfigurationRoot BuildConfiguration(string configFilePath) {
         IConfigurationBuilder builder = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile(configFilePath, optional: false, reloadOnChange: true);
-        IConfigurationRoot configuration = builder.Build();
+        return builder.Build();
+    }
 
+    private static IModule LoadModule(IConfigurationRoot configuration) {
         // Read configuration values
-        string? moduleLoaderType = configuration["ModuleLoader:Type"];
+        string moduleLoaderType = configuration["ModuleLoader:Type"] ?? throw new InvalidOperationException("ModuleLoader type must be specified.");
+        string pathToLoad = GetPathToLoad(configuration, moduleLoaderType);
+
+        // Determine and use the loader based on configuration
+        IModuleLoader loader = GetModuleLoader(moduleLoaderType);
+        return loader.LoadModule(pathToLoad);
+    }
+
+    private static string GetPathToLoad(IConfigurationRoot configuration, string moduleLoaderType) {
         string? filePath = configuration["ModuleLoader:FilePath"];
         string? folderPath = configuration["ModuleLoader:FolderPath"];
 
-        // Determine the loader based on configuration
-        IModuleLoader loader = moduleLoaderType switch {
-            "SingleFile" => new SingleFileModuleLoader(),
-            "Folder" => new FolderModuleLoader(),
-            _ => throw new InvalidOperationException("Unknown ModuleLoader type specified in configuration.")
-        };
-
-        // Use the appropriate file path based on loader type
         string? pathToLoad = moduleLoaderType == "SingleFile" ? filePath : folderPath;
 
         if (string.IsNullOrEmpty(pathToLoad)) {
             throw new InvalidOperationException("The path to load the module cannot be null or empty.");
         }
 
-        IModule rootModule = loader.LoadModule(pathToLoad);
+        return pathToLoad;
+    }
 
+    private static IModuleLoader GetModuleLoader(string moduleLoaderType) {
+        return moduleLoaderType switch {
+            "SingleFile" => new SingleFileModuleLoader(),
+            "Folder" => new FolderModuleLoader(),
+            _ => throw new InvalidOperationException($"Unknown ModuleLoader type: {moduleLoaderType}")
+        };
+    }
+
+    private static Compiler BuildCompiler(IModule rootModule) {
         IParser parser = new Parser();
         ILexer lexer = new Lexer();
-
         ISymbolTable symbolTable = new SymbolTable();
         ISymbolTableBuilder symbolTableBuilder = new SymbolTableBuilder();
 
-        // Create and return the compiler instance
         return new Compiler(rootModule, parser, lexer, symbolTable, symbolTableBuilder);
     }
 }
